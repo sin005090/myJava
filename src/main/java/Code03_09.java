644cf006bbe3622b37c4955aef89c7e8339a1b5d
Hello2 <sin005090@gmail.com>
2026-03-27 13:41:42 +0900

    Code03_09

---
+++ src/main/java/Code03_09.java
@@ -0,0 +1,12 @@
+public class Code03_09 {
+    public static void main(String[] args){
+        int num = 20;
+        num++ ; System.out.println(num+" ");
+        num-- ; System.out.println(num+" ");
+        num += 3 ; System.out.println(num+" ");
+        num -= 3 ; System.out.println(num+" ");
+        num *= 3 ; System.out.println(num+" ");
+        num /= 3 ; System.out.println(num+" ");
+        num %= 3 ; System.out.println(num+" ");
+    }
+}
