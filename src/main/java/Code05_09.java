c7cf61278151ddb491904c994ebf161cc2ee6ae5
sin005090 <sin005090@gmail.com>
2026-04-10 15:29:17 +0900

    Code05_09

--- src/main/java/Code05_09.java
+++ src/main/java/Code05_09.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class Code05_09 {
+public class
+Code05_09 {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int select;
