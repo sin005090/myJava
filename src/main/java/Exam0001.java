805506b3bddb6f49f62b67e22b6ec20233d50fc9
sin005090 <sin005090@gmail.com>
2026-04-17 12:23:22 +0900

    Exam_max

---
+++ src/main/java/Exam0001.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Exam0001 {
+    public static void main(String[] args){
+
+        int[] nums = {1, 3, 7, 1, 4, 7, 3, 1, 9, 0, 7, 3};
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        System.out.println("출력결과 : " + max + "입니다.");
+    }
+}
